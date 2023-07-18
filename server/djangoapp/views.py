ca75617f60b6dbf21562c9d58d284b0a8fb8409f
Tusshar10 <131467401+Tusshar10@users.noreply.github.com>
2023-07-18 18:46:08 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf,get_dealer_reviews_from_cf
 import logging
 import json
 
@@ -90,8 +90,15 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request,dealer_id):
+    if request.method == "GET":
+        context = {}
+        review_url = "https://eu-gb.functions.appdomain.cloud/api/v1/web/e81514e9-de36-4069-8e3b-01724008e3b0/dealership-package/get-review"
+        reviews = get_dealer_reviews_from_cf(review_url,dealer_id=dealer_id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
